a5260e0314a020c42003244cfbf526a2e7cbbc1c
Thaksin <thaksin@THAKSINs-MacBook-Pro.local>
2024-12-05 17:32:27 +0700

    0.0.0.0 for deploy

--- server/index.js
+++ server/index.js
@@ -44,6 +44,6 @@ app.use((err, req, res, next) => {
 });
 
 // Start Server
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server is running on http://0.0.0.0:${PORT}`);
 });
